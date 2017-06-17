32b6696441969ae8d0e8e6a736311bd00ba490db
Mitch Robb <olordhux+github@gmail.com>
2017-06-17 14:08:26 -0700

    init, fill canvas

---
+++ src/fast_timer/core.cljs
@@ -0,0 +1,18 @@
+(ns fast-timer.core
+  (:require [monet.canvas :as canvas]))
+
+(enable-console-print!)
+
+(defn on-js-reload [])
+(defonce app-state (atom {:text "Hello world!"}))
+
+(def canvas-dom (.getElementById js/document "canvas"))
+(def monet-canvas (canvas/init canvas-dom "2d"))
+
+(canvas/add-entity monet-canvas :background
+                   (canvas/entity {:x 0 :y 0 :w 600 :h 600} ; val
+                                  nil                       ; update function
+                                  (fn [ctx val]             ; draw function
+                                    (-> ctx
+                                        (canvas/fill-style "#191d21")
+                                        (canvas/fill-rect val)))))
